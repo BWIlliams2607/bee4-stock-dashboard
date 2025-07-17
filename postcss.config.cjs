40e0ad7a627a31a1457ada6f2480563f5677bd3e
DonaldDuckX1 <146040036+DonaldDuckX1@users.noreply.github.com>
2025-07-17 16:17:26 +0100

    t

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,7 @@
 // postcss.config.cjs
-/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    tailwindcss: {},    // use the core plugin directly
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
