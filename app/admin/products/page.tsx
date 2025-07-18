4fde6dfc8aadeaf62311e3d00ae20e277da48947
DonaldDuckX1 <146040036+DonaldDuckX1@users.noreply.github.com>
2025-07-18 10:42:30 +0100

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect, useRef, Fragment } from "react"
+import { useState, useEffect, useRef } from "react"
 import { Combobox } from "@headlessui/react"
 import { motion } from "framer-motion"
 import { CameraBarcodeScanner } from "@/components/CameraBarcodeScanner"
