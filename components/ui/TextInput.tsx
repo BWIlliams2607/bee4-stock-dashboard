9589bf254a373eab6ee25a5930900d621b8a6e8a
DonaldDuckX1 <146040036+DonaldDuckX1@users.noreply.github.com>
2025-07-21 15:04:53 +0100

    t

--- components/ui/TextInput.tsx
+++ components/ui/TextInput.tsx
@@ -1,11 +1,17 @@
 // components/ui/TextInput.tsx
 import React from "react";
 
-export function TextInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
+export const TextInput = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
   return (
     <input
+      ref={ref}
       {...props}
       className="h-12 rounded-lg border border-gray-700 px-4 bg-gray-700 text-sm placeholder-gray-400 text-white focus:ring-2 focus:ring-green-500 w-full"
     />
   );
-}
+});
+
+TextInput.displayName = "TextInput";
